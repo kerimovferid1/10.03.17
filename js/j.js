a9d5217e83afd0e94912087aa09e3a85039af6f2
Ferid <kerimovferid98@gmail.com>
2017-03-10 09:28:01 +0400

    ddf

--- js/j.js
+++ js/j.js
@@ -1,4 +1,52 @@
 function slider(val) {
 	var attr=val.getElementsByTagName("img")[0].getAttribute("src");
-	document.getElementById("slider")[1].setAttribute("style","background-image:url("+attr+")");
+	document.getElementsByClassName("slider")[0].setAttribute("style","background-image:url("+attr+")");
+	    // document.getElementsByTagName("img")[1].style.boxSizing="borderBox";
+	    // document.getElementsByTagName("img")[1].style.border="7px solid white"
+	      
+	    //   document.getElementsByTagName("img")[2].style.boxSizing="borderBox";
+	    // document.getElementsByTagName("img")[2].style.border="7px solid white"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function openNav() {
+    document.getElementsByClassName("hR").style.height = "400px";
+}
+
+
+function closeNav() {
+	var mq = window.matchMedia("(max-width: 760px)");
+
+
+if (mq.matches) {
+    document.getElementsByClassName("hR")[0].style.display = "none";
+}
+ else {
+
+    document.getElementsByClassName("hR")[0].style.display.backgroundColor = "green";
+}
+}
+function k(){
+	document.getElementsByClassName("hR")
 }
