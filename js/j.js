7f18b26b7dea81ddce578daee39373a66c42ff34
Ferid <kerimovferid98@gmail.com>
2017-03-14 09:17:17 +0400

    ddf

--- js/j.js
+++ js/j.js
@@ -1,52 +1,84 @@
 function slider(val) {
-	var attr=val.getElementsByTagName("img")[0].getAttribute("src");
-	document.getElementsByClassName("slider")[0].setAttribute("style","background-image:url("+attr+")");
-	    // document.getElementsByTagName("img")[1].style.boxSizing="borderBox";
-	    // document.getElementsByTagName("img")[1].style.border="7px solid white"
-	      
-	    //   document.getElementsByTagName("img")[2].style.boxSizing="borderBox";
-	    // document.getElementsByTagName("img")[2].style.border="7px solid white"
+    var attr = val.getElementsByTagName("img")[0].getAttribute("src");
+    document.getElementsByClassName("slider")[0].setAttribute("style", "background-image:url(" + attr + ")");
+    for (var i = 0; i < document.getElementsByClassName("js").length; i++) {
+        var element = document.getElementsByClassName("js")[i];
+        element.className = element.className.replace("otherclass", "");
+
+    }
+    if (!val.classList.contains('otherclass')) {
+        val.className += " otherclass";
+    }
+    var elements = document.getElementsByClassName('js');
+    for (i = 0; i < elements.length; i++) {
+        if (elements[i].classList.contains('otherclass')) {
+            console.log(i);
+            var el = document.getElementsByClassName('rM')[i]
+            sli(el)
+            break;
+        }
+
+    }
+
+}
+
+function clickNext() {
+    var elements = document.getElementsByClassName('js');
+    for (i = 0; i < elements.length; i++) {
+        if (elements[i].classList.contains('otherclass')) {
+            if (i == (elements.length - 1)) {
+                slider(elements[0])
+
+
+            } else {
+                slider(elements[i + 1])
+
+            }
+            break;
+        }
+    }
+}
+
+function clickPrev() {
+    var elements = document.getElementsByClassName('js');
+    for (i = 0; i < elements.length; i++) {
+        if (elements[i].classList.contains('otherclass')) {
+            console.log(elements[i].children);
+            if (i == 0) {
+                slider(elements[elements.length - 1])
+
+            } else {
+                slider(elements[i - 1])
+
+
+            }
+            break;
+        }
+    }
+}
+
+function kh() {
+    document.getElementsByClassName('b')[0].style.display = "none";
+}
+
+function sli(element) {
+    for (var i = 0; i < document.getElementsByClassName("rM").length; i++) {
+        var elementss = document.getElementsByClassName("rM")[i];
+        elementss.className = elementss.className.replace("activated", "");
+
+    }
+    element.className += " activated";
+
 }
 
 
+function menu(m){
+	  for (var i = 0; i < document.getElementsByClassName("hR").length; i++) {
+        var element = document.getElementsByClassName("hR")[i];
+        element.className = element.className.replace("other", "");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-function openNav() {
-    document.getElementsByClassName("hR").style.height = "400px";
-}
-
-
-function closeNav() {
-	var mq = window.matchMedia("(max-width: 760px)");
-
-
-if (mq.matches) {
-    document.getElementsByClassName("hR")[0].style.display = "none";
-}
- else {
-
-    document.getElementsByClassName("hR")[0].style.display.backgroundColor = "green";
-}
-}
-function k(){
-	document.getElementsByClassName("hR")
+    }
+    if (!m.classList.contains('other')) {
+        m.className += " other";
+    }
 }
